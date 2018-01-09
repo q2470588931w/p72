b595773af14d2357b5eba6a44bd61ace91be2394
q2470588931w <2470588931@qq.com>
2018-01-09 10:45:06 +0800

    take views_c

--- mymvc/application/views_c/%%97^978^97868B9C%%password.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.31, created on 2017-12-26 16:34:48
-         compiled from myPass/password.tpl */ ?>
-旧时密码：<input type="password" name="old" id="old"><br>
-全新密码：<input type="password" name="new" id="new"><br>
-确认密码：<input type="password" name="new2" id="new2"><br>
